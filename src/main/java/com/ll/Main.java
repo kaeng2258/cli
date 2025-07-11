d200277a667f05b71512abd2e1a63fedb7d36446
깃íkaeng2258 <kaeng2258@gmail.com>
2025-07-11 18:07:49 +0900

    2단ãã계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -3,24 +3,55 @@ package com.ll;
 import java.util.Scanner;
 
 public class Main {
+    static String name;
+    static String text;
+    static Scanner sc = new Scanner(System.in);
+    static String command = sc.nextLine();
     public static void main(String[] args) {
-
-        System.out.println("====게시판앱====");
-        System.out.println("1 입력시 프로그램 종료");
-        Scanner sc = new Scanner(System.in);
-
-
+        loof();
+    }
+    static void loof(){
         while(true){
-            System.out.printf("명령)");
-            String command = sc.nextLine();
+            print();
             if (command.equals("종료")) {
-                sc.close();
-                System.exit(0);
+                close();
+            }
+            else if (command.equals("등록")){
+                insert();
+            }
+            else if (command.equals("출력")){
+                insert_output();
+            }
+            else{
+                System.out.println("잘못입력하셨습니다.");
+                continue;
             }
         }
     }
-   public void close(){
-
+    static void print(){
+        System.out.println("====게시판앱====");
+        System.out.println("명령어");
+        System.out.println("종료");
+        System.out.println("등록");
+        System.out.println("출력");
+        System.out.printf("입력칸 : ");
+    }
+   static void close(){
+       sc.close();
+       System.exit(0);
 
     }
+    static void insert(){
+        System.out.printf("제목 : ");
+        name = sc.nextLine();
+        System.out.printf("내용 : ");
+        text = sc.nextLine();
+        loof();
+    }
+    static void insert_output(){
+        System.out.println("제목 : " + name);
+        System.out.println("내용 : " + text);
+        loof();
+    }
 }
+
