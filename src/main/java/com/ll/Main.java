8394c6b61f2b27e44fee48646cbeb1d7fe9e5904
깃íkaeng2258 <kaeng2258@gmail.com>
2025-07-11 17:29:33 +0900

    1단계

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,11 +1,26 @@
 package com.ll;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+        System.out.println("====게시판앱====");
+        System.out.println("1 입력시 프로그램 종료");
+        Scanner sc = new Scanner(System.in);
+
+
+        while(true){
+            System.out.printf("명령)");
+            String command = sc.nextLine();
+            if (command.equals("종료")) {
+                sc.close();
+                System.exit(0);
+            }
         }
+    }
+   public void close(){
+
+
     }
 }
